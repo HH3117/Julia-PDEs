f6557c8be8f4983aa036301e67e5acfec27c9b97
HAO HAO <HH3117@ic.ac.uk>
2019-07-15 14:54:52 -0400

    Reduce allocations

--- pdes.jl
+++ pdes.jl
@@ -14,16 +14,16 @@ Ti = ApproxFun.plan_itransform(S, n)
 û₀ = T*cos.(cos.(x.-0.1))
 A = 0.01*D2
 tmp = similar(û₀)
-p = (D,D2,T,Ti,tmp,similar(tmp),similar(tmp))
+p = (D,D2,T,Ti,tmp,similar(tmp))
 function burgers(dû,û,p,t)
-    D,D2,T,Ti,u,up,tmp = p
-    mul!(u, Ti, û)
+    D,D2,T,Ti,u,tmp = p
     mul!(tmp, D, û)
-    mul!(up, Ti, tmp)
-    @. tmp = u*up
-    mul!(up, T, tmp)
+    mul!(u, Ti, tmp)
+    mul!(tmp, Ti, û)
+    @. tmp = tmp*u
+    mul!(u, T, tmp)
     mul!(tmp, A, û)
-    @. dû = tmp - up
+    @. dû = tmp - u
 end
 
 prob = ODEProblem(burgers, û₀, (0.0,5.0), p)
@@ -35,48 +35,47 @@ plot!(x, Ti*û(2.0))
 plot!(x, Ti*û(3.0))
 plot!(x, Ti*û(5.0))
 
+
 # Kuramoto-Sivashinsky
 D4 = Derivative(S,4)[1:n,1:n]
-q = (D,D2,T,Ti,D4)
-Len=2*pi/0.025
-η1=min(x/Len,0.1.-x/Len)
-η2=20*(x/Len.-0.2).*(0.3.-x/Len)
-η3=min(x/Len.-0.6,0.7.-x/Len)
-η4=min(x/Len.-0.9,1 .-x/Len)
-u₀=zeros(n)
-for i =1:n
-     u₀[i] = 16*max(0,η1[i],η2[i],η3[i],η4[i])
-end
-û₀=T*u₀
-
+û₀ = T*(cos.(x./16).*(1 .+ sin.(x./2.04)))
+plot(x,Ti*û₀)
+tmp=similar(û₀)
+q = (D,D2,T,Ti,D4,tmp,similar(tmp),similar(tmp))
 function kuramoto_sivashinsky(dû,û,q,t)
-    D,D2,T,Ti,D4 = q
-    u = Ti*û
-    up =T*(u.*u)
-    dû .= .-D4*û .-D2*û .-(D*up)/2
+    D,D2,T,Ti,D4,tmp,u,uc = q
+    mul!(u, D, û)
+    mul!(tmp, Ti, u)
+    mul!(u, Ti, û)
+    @. tmp=tmp*u
+    mul!(u,T, tmp)
+    mul!(tmp, D4, û)
+    mul!(uc, D2, û)
+    @. dû = - tmp - uc - u
 end
 
-
-prob = ODEProblem(kuramoto_sivashinsky, û₀, (0.0,5.0), q)
+prob = ODEProblem(kuramoto_sivashinsky, û₀, (0.0,300.0), q)
 @time û  = solve(prob, CVODE_BDF(); reltol=1e-5,abstol=1e-5)
-plot(x,u₀)
-for t in 0:0.5:5
+for t in 0:50:300
     plt=(plot(x, Ti*û(t)))
-    ylims!(0.0, 0.4)
+    ylims!(1., 2.3)
     display(plt)
-    sleep(0.02)
+    sleep(0.01)
 end
 
 
 # Allen-Cahn
-w = (D2,T,Ti)
 û₀ = T*cos.(cos.(x.-0.1))
 ϵ=1/3
+tmp = similar(û₀)
+w = (D2,T,Ti,tmp,similar(tmp),similar(tmp))
 function allen_cahn(dû,û,w,t)
-    D2,T,Ti = w
-    u = Ti*û
-    uc=u.^3
-    dû .= 10*D2*û .+ (T*(uc)-û)/(ϵ^2)
+    D2,T,Ti,tmp,u,uc = w
+    mul!(u,Ti,û)
+    @.uc = u^3
+    mul!(u,T,uc)
+    mul!(tmp,D2,û)
+    @.dû = 10*tmp + (u-û)/(ϵ^2)
 end
 
 prob = ODEProblem(allen_cahn, û₀, (0.0,5.0), w)
@@ -99,26 +98,26 @@ for t in 0:0.01:0.2
     display(plt)
     sleep(0.05)
 end
-plot(x,Ti*û₀)
 
 #Korteweg–de Vries (kdv) equation
 D3  = (Derivative(S,3) → S)[1:n,1:n]
-p = (D,D3,T,Ti)
-#û₀ = T*cos.(pi .*x)
 û₀ = T*cos.(x)
 δ=0.022
+tmp = similar(û₀)
+p = (D,D3,T,Ti,similar(tmp),tmp)
 function kdv(dû,û,p,t)
-    D,D3,T,Ti = p
-    u = Ti*û
-    up = Ti*(D*û)
-    dû .= .-(D3*û) .+6 .* (T*(u.*up))
-    #dû .= .-((δ^2) .*(D3*û) .+ T*(u.*up))
+    D,D3,T,Ti,u,tmp = p
+    mul!(u,D,û)
+    mul!(tmp,Ti,u)
+    mul!(u,Ti,û)
+    @. tmp=u*tmp
+    mul!(u,T,tmp)
+    mul!(tmp,D3,û)
+    @.dû = -tmp + 6*u
 end
 
 prob = ODEProblem(kdv, û₀, (0.0,1.0), p)
 @time û  = solve(prob, CVODE_BDF(); reltol=1e-5,abstol=1e-5) #59s
-
-plot(x,Ti*û₀)
 
 plot(x, Ti*û(0.0))
 plot!(x, Ti*û(0.2))
