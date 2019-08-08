8fac10239f4349eba5a25a959cabb740aa931d2f
Hao Hao <hh3117@ic.ac.uk>
2019-08-08 17:12:51 -0400

    WORKS

--- Neural Network/Mixed neural PDE.jl
+++ Neural Network/Mixed neural PDE.jl
@@ -18,28 +18,24 @@ D2=diagm(-1 => dl, 0 => -2*d, 1 => dl)
 D2new=hcat(zv,D2,zv)
 D2new[1,1]=D2new[end,end]=1
 D2now=(ϵ/(dx^2)) .*D2new
-D2now=convert(Array{Float32},D2now)
 #D1 first order upwind
 ϵ2=0.5
 D1=diagm(-1 => -dl, 0 => d)
 D1new=hcat(zv,D1,zv)
 D1new[1,1]=-1
 D1now=(ϵ2)*(1/dx) .* D1new
-D1now=convert(Array{Float32},D1now)
 #boundary condition matrix QQ
 Q=Matrix{Int64}(I,N-2,N-2)
 QQ=vcat(zeros(1,N-2),Q,zeros(1,N-2))
-QQ=convert(Array{Float32},QQ)
 #Initial conditions
 xs = (1:N) * dx
 x = xs[2:N-1]
-f0 = x -> Float32(exp(-200*(x-0.75)^2))
+f0 = x -> exp(-200*(x-0.75)^2)
 u0 = f0.(xs)
 plot(xs,u0)
 #get true_sol
 u0=u0[2:N-1]
 r=zeros(N-2)
-r=convert(Vector{Float32},r)
 r=[0;r;0]
 p =D1now,D2now,QQ,similar(r),r
 function bur(du,u,p,t)
@@ -77,7 +73,7 @@ function predict_n_ode()
 end
 loss_n_ode() = sum(abs2,ode_data .- predict_n_ode())
 #callback and train
-data = Iterators.repeated((), 10)
+data = Iterators.repeated((), 200)
 opt = ADAM(0.1)
 pre_pts=zeros(datasize)
 cb = function ()
